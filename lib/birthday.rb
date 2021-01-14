24e62aca21d73aceb79d575eacfd36535f6184e9
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-01-14 15:09:38 -0500

    commit tree 6b35834e421fd1c9d44ea57f87c0aaa9e8e523fb

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each do |name, age|
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+  end
 end
 
 
